33fe1d3aeced77ccec5fdf276bf9c604ad90cbf2
unknown <paulartusso@gmail.com>
2020-12-25 20:00:15 -0300

    trying animations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,10 @@
-let showCards = () =>{
+const showCards = () =>{
     $("#hidden-div").slideToggle("slow");
     let button = $("#cards-button");
     button.html() == "VER MENOS" ? button.html("VER MÁS") : button.html("VER MENOS");
 }
 
+const bounce = () =>{
+    let scroll = document.querySelector(".scroll");
+    scroll.classList.add("bounce");
+} 
